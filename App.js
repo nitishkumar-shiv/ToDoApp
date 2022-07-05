9b9489fd5c52df5007d61ab4b15ae3c0848a1e16
nitishkumar.shiv <nitishkumar.shivnandan@rently.com>
2022-07-05 14:51:15 +0530

    textinputview and add button created

--- App.js
+++ App.js
@@ -1,11 +1,28 @@
 import React from "react";
-import { SafeAreaView, Text, StyleSheet, View } from "react-native";
+import { SafeAreaView, Text, StyleSheet, View, TextInput, TouchableOpacity } from "react-native";
 
 const App = () => {
+
+  const onPressFunction = () => {
+
+  }
+
+
   return (
     <SafeAreaView style={styles.container}>
       <View style={styles.headingView}>
         <Text style={styles.headingText}>TO~DO~APP</Text>
+      </View>
+      <View style={styles.inputView}>
+        <TextInput
+          style={styles.inputText}
+          placeholder="what do you want to do?"
+          placeholderTextColor={"#fff8dc"}
+        />
+        <TouchableOpacity style={styles.button} onPress={onPressFunction}>
+          <Text style={styles.buttonText}>+</Text>
+        </TouchableOpacity>
+
       </View>
     </SafeAreaView>
   );
@@ -32,7 +49,42 @@ const styles = StyleSheet.create(
     headingText: {
       fontSize: 30,
       color: "#fff8dc"
+    },
+
+    inputView: {
+      flex: 1 / 8,
+      backgroundColor: "#0000ff",
+      margin: 3,
+      borderRadius: 15,
+      alignItems: "center",
+      flexDirection: "row",
+    },
+
+    inputText: {
+      borderWidth: 2,
+      borderColor: "#fff8dc",
+      height: 40,
+      width: 300,
+      marginLeft: 3,
+
+    },
+
+    button: {
+      marginLeft: 15,
+      backgroundColor: "#00ffff",
+      height: 50,
+      width: 50,
+      borderRadius: 25,
+      justifyContent: "center",
+      alignItems: "center",
+    },
+
+    buttonText: {
+      fontSize: 30,
+      fontWeight: "bold",
+      color: "#0000ff"
     }
+
   }
 )
 
